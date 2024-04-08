16e4028f0d2cbfb59e0a6c9642f964e6563d96be
武迪 <wudi07@qianxin.com>
2024-04-08 20:25:43 +0800

    反转链表

---
+++ 206.反转链表.go
@@ -0,0 +1,37 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/*
+ * @lc app=leetcode.cn id=206 lang=golang
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+
+	cur := head
+	for cur != nil {
+		//拿到下一个节点
+		nextTmp := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nextTmp
+	}
+	return pre
+}
+
+// @lc code=end
