5ec8ad09bb5fcbb2e071f8c5750f6c2eedeb1804
FiveCubes <diwuwudi123@outlook.com>
2024-07-24 09:41:59 +0800

    优化移动0的时间复杂度

--- 283.移动零.go
+++ 283.移动零.go
@@ -19,14 +19,10 @@ func moveZeroes(nums []int) {
 	slow, fast := 0, 0
 	for fast < len(nums) {
 		if nums[fast] != 0 {
-			nums[slow] = nums[fast]
+			nums[fast], nums[slow] = nums[slow], nums[fast]
 			slow++
 		}
 		fast++
-	}
-	for slow < len(nums) {
-		nums[slow] = 0
-		slow++
 	}
 
 }
